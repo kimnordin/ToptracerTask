acfea15224ba939e5a8c37f4136a81448141fe13
kimnordin <kim.nordin97@gmail.com>
2022-05-24 18:04:39 +0200

    Initial Commit

---
+++ ToptracerTask/ToptracerTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToptracerTaskApp.swift
+//  ToptracerTask
+//
+//  Created by Kim Nordin on 2022-05-24.
+//
+
+import SwiftUI
+
+@main
+struct ToptracerTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
